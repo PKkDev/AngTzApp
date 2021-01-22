b2a89422e342ac3f15cbf323f95c380003b440a8
Kirill <portnov-kiril11997@mail.ru>
2021-01-22 17:41:29 +0400

    update

--- client-app/src/environments/environment.ts
+++ client-app/src/environments/environment.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: false,
-  urlApi: 'http://localhost:5001/',
-  openApiUrl: 'http://localhost:5001/swagger/index.html'
+  // urlApi: 'http://localhost:5001/',
+  // openApiUrl: 'http://localhost:5001/swagger/index.html'
+  urlApi: 'https://testfortest.ru.com/',
+  openApiUrl: 'https://testfortest.ru.com/swagger'
 };
